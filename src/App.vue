d85585536f798acdbfc1a8867b47641959af1ac0
RomainGuarinoni <romain.guar91@gmail.com>
2020-12-29 17:55:35 +0100

    test scroll auto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="first">
+      <h1>Scroll vers le bas</h1>
+    </div>
+    <div class="second">
+      <h1>Scroll vers le haut</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  position: fixed;
+
+  overflow-y: scroll;
+  overflow-x: hidden;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  scroll-snap-type: y mandatory;
+}
+::-webkit-scrollbar {
+  width: 0;
+}
+h1 {
+  padding: 0;
+  margin: 0;
+  color: white;
+}
+.first {
+  width: 100%;
+  height: 100%;
+  background: blue;
+  scroll-snap-align: start;
+  scroll-snap-stop: normal;
+}
+.second {
+  width: 100%;
+  height: 100%;
+  background: red;
+  scroll-snap-align: start;
+  scroll-snap-stop: normal;
+}
+.first,
+.second {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
