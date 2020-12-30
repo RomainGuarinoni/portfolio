1c9db18a44127f1af9f59b64f5278d0d816c2b72
RomainGuarinoni <romain.guar91@gmail.com>
2020-12-30 13:53:31 +0100

    test websticky position

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <div class="first">
-      <h1>Scroll vers le bas</h1>
+    <div class="sticky">
+      <p>yyeaahh</p>
     </div>
-    <div class="second">
-      <h1>Scroll vers le haut</h1>
+    <div class="test">
+      <div class="first">
+        <h1>Scroll vers le bas</h1>
+        <p><a href="#test">Hello</a></p>
+      </div>
+      <div class="second">
+        <h1 id="test">test</h1>
+      </div>
     </div>
   </div>
 </template>
@@ -22,15 +28,43 @@ export default {
   margin: 0;
   padding: 0;
   height: 100vh;
-  position: fixed;
-
+  position: absolute;
+  scroll-behavior: smooth;
   overflow-y: scroll;
   overflow-x: hidden;
   text-align: center;
   scroll-snap-type: y mandatory;
 }
+.test {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  position: absolute;
+  scroll-behavior: smooth;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  text-align: center;
+  z-index: 1;
+  scroll-snap-type: y mandatory;
+}
 ::-webkit-scrollbar {
   width: 0;
+}
+a {
+  color: white;
+}
+.sticky {
+  color: rgba(9255, 255, 255);
+  background: rgba(160, 35, 35, 0.116);
+  top: 0px;
+  z-index: 2;
+  position: -webkit-sticky !important;
+  position: sticky !important;
+}
+.nav {
+  position: -webkit-sticky;
+  position: sticky;
 }
 h1 {
   padding: 0;
@@ -51,10 +85,13 @@ h1 {
   scroll-snap-align: start;
   scroll-snap-stop: normal;
 }
-.first,
-.second {
+
+.second,
+.first {
   display: flex;
   flex-direction: column;
+  position: relative;
+  z-index: 1;
   justify-content: center;
 }
 </style>
